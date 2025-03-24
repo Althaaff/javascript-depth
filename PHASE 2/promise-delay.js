0b97519a8f0d5b403559990c8ed5b195b4262a80
Althaaff <muhammadalthaf228@gmail.com>
2025-03-24 14:28:15 +0530

    implement a promise-based delay function (sleep)

--- PHASE 2/promise-delay.js
+++ PHASE 2/promise-delay.js
@@ -1,17 +1,17 @@
-// function sleep(ms) {
-//   // ms means miliseconds
+function sleep(ms) {
+  // ms means miliseconds
 
-//   // above function returns promise :
-//   return new Promise((resolve, reject) => {
-//     setTimeout(resolve, ms);
-//   });
-// }
+  // above function returns promise :
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-// console.log("Start!");
-// sleep(2000)
-//   .then(() => console.log("after 2 seconds Boom!"))
-//   .catch(() => console.log("promise failed!"));
-// console.log("End!");
+console.log("Start!");
+sleep(2000)
+  .then(() => console.log("after 2 seconds Boom!"))
+  .catch(() => console.log("promise failed!"));
+console.log("End!");
 
 // challenge :
 // 🔹 Can you modify sleep(ms) to return both elapsed time and current time when it resolves?
@@ -27,12 +27,10 @@ function sleep(ms) {
     }, ms);
   });
 }
-sleep(5000).then(({ elapsedTime, currentTime }) => {
+sleep(1000).then(({ elapsedTime, currentTime }) => {
   console.log(`elapsed time: ${elapsedTime} ms`);
   console.log(`current time: ${new Date(currentTime).toLocaleTimeString()}`);
 });
 // sleep(2000).then(() => {
 //   console.log("Hello Javascript!");
 // });
-
-// challenge 2 :
