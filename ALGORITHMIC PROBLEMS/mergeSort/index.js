9696df4334356a42dca2359cd1dcf5a60a60dbd5
Althaaff <muhammadalthaf228@gmail.com>
2025-04-04 22:54:14 +0530

    implement numberOfInversions

--- ALGORITHMIC PROBLEMS/mergeSort/index.js
+++ ALGORITHMIC PROBLEMS/mergeSort/index.js
@@ -46,3 +46,23 @@ function merge(left, right) {
 }
 
 console.log(mergeSort([4, 3, 2, 9, 8, 6, 1, -1, 0]));
+
+// find numberOfInversions :
+function numberOfInversions(a) {
+  let cnt = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    for (let j = i + 1; j < a.length; j++) {
+      if (a[i] > a[j]) {
+        console.log(a[i]);
+        cnt++;
+      }
+    }
+  }
+
+  return cnt;
+}
+
+const a = [5, 4, 3, 2, 1, 2];
+
+console.log(numberOfInversions(a));
