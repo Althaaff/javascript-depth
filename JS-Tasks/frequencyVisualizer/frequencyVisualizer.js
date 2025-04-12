61068ddba931144e2ab7dc9b597f038f7759f791
Althaaff <muhammadalthaf228@gmail.com>
2025-04-12 23:22:54 +0530

    implement frequency visualizer using vanilla JS

---
+++ JS-Tasks/frequencyVisualizer/frequencyVisualizer.js
@@ -0,0 +1,45 @@
+const inputText = document.getElementById("inputText");
+const frequencyDisplay = document.getElementById("frequency");
+
+inputText.addEventListener("input", showFrequency);
+
+function showFrequency() {
+  const text = inputText.value;
+  const freqMap = {};
+  const hasValidChar = false;
+
+  for (let char of text) {
+    if (char.match(/[a-z0-9]/)) {
+      freqMap[char] = (freqMap[char] || 0) + 1;
+
+      hasValidChar = true;
+    }
+  }
+
+  if (!hasValidChar) {
+    console.log("true or false", hasValidChar);
+    const error = document.createElement("span");
+
+    error.textContent = "Please Enter characters and numbers!";
+
+    frequencyDisplay.appendChild(error);
+  }
+
+  frequencyDisplay.innerHTML = "";
+
+  // Display each character and its frequency :
+  for (let char in freqMap) {
+    let count = freqMap[char];
+
+    let item = document.createElement("div");
+    item.className = "frequency-item";
+
+    item.innerHTML = `
+     <span class="char">${char}</span>
+      <div class="bar" style="width: ${count * 20}px;"></div>
+      <span>(${count})</span>
+    `;
+
+    frequencyDisplay.appendChild(item);
+  }
+}
