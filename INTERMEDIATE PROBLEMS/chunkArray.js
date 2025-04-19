bd68e2ec25a5d13b2d402fcf0146b086d756f9c9
Althaaff <muhammadalthaf228@gmail.com>
2025-04-19 23:48:42 +0530

    Add solution to Split an array into chunks (subarrays) of size k .

---
+++ INTERMEDIATE PROBLEMS/chunkArray.js
@@ -0,0 +1,21 @@
+function makeChuckArray(arr, k) {
+  const chunks = [];
+
+  for (let i = 0; i < arr.length; i += k) {
+    let tempChunk = [];
+
+    for (j = i; j < i + k && j < arr.length; j++) {
+      tempChunk.push(arr[j]);
+    }
+
+    chunks.push(tempChunk);
+  }
+
+  return chunks;
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+const chunked = makeChuckArray(arr, 3);
+
+console.log(chunked);
