381e46b188009e64a70bca4dbff24eb97af0e43e
Althaaff <muhammadalthaf228@gmail.com>
2025-05-17 13:40:54 +0530

    implementing stack using JS array .

--- DS/stack.js
+++ DS/stack.js
@@ -56,15 +56,74 @@ function Stack() {
 }
 
 // Using the Stack :
-let stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
+// let stack = new Stack();
+// stack.push(1);
+// stack.push(2);
+// stack.push(3);
 
-console.log(stack.peek());
-console.log(stack.display());
-console.log(stack.isEmpty());
-console.log(stack.pop());
-console.log(stack.size());
-console.log(stack.clear());
-console.log(stack.isEmpty());
+// console.log(stack.peek());
+// console.log(stack.display());
+// console.log(stack.isEmpty());
+// console.log(stack.pop());
+// console.log(stack.size());
+// console.log(stack.clear());
+// console.log(stack.isEmpty());
+
+// Stack data structure in JavaScript with Array :
+
+function Stack() {
+  let items = [];
+
+  // Push an item in the Stack :
+  this.push = function (element) {
+    items.push(element);
+  };
+
+  // display pushed elements from the stack :
+  this.display = function () {
+    let pushedElements = "";
+    for (let i = 0; i < items.length; i++) {
+      pushedElements += items[i] + " ";
+    }
+
+    return pushedElements.trim();
+  };
+
+  // Pop an item from the Stack :
+  this.pop = function () {
+    return items.pop();
+  };
+
+  // Peek top item from the Stack :
+  this.peek = function () {
+    return items[items.length - 1];
+  };
+
+  // Check if Stack is empty :
+  this.isEmpty = function () {
+    return items.length === 0;
+  };
+
+  // Clear the Stack :
+  this.clear = function () {
+    items.length = 0;
+  };
+
+  // Size of the stack :
+  this.size = function () {
+    return items.length;
+  };
+}
+
+const stack2 = new Stack();
+stack2.push(4);
+stack2.push(5);
+stack2.push(6);
+
+console.log(stack2.display());
+console.log(stack2.pop());
+console.log(stack2.isEmpty());
+console.log(stack2.peek());
+console.log(stack2.size());
+stack2.clear();
+console.log(stack2.isEmpty());
