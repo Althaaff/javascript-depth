1b9e7caee7559061f2d2c17e4aeaaf22ccce1803
Althaaff <muhammadalthaf228@gmail.com>
2025-06-02 23:45:55 +0530

    add solution to reverse integer problem

---
+++ LeetCode/reverseInteger.js
@@ -0,0 +1,21 @@
+// leetcode medium problem :
+
+var reverse = function (x) {
+  function helper(x, rev) {
+    if (x === 0) return rev;
+    if (rev > Math.trunc(2147483647 / 10) || rev < Math.trunc(-2147483648 / 10))
+      return 0;
+
+    let rem = x % 10;
+    x = Math.trunc(x / 10);
+    rev = rev * 10 + rem;
+
+    return helper(x, rev);
+  }
+
+  return helper(x, 0);
+};
+
+console.log(reverse(123));
+console.log(reverse(-123));
+console.log(reverse(345));
