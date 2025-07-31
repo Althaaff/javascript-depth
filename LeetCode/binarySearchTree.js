d95f5642c4cd8c3efe2ea3a51d8988352565acd3
Althaaff <muhammadalthaf228@gmail.com>
2025-07-31 10:23:59 +0530

    Add solution for converting binary search tree to array

--- LeetCode/binarySearchTree.js
+++ LeetCode/binarySearchTree.js
@@ -1,24 +1,3 @@
-// class TreeNode {
-//   constructor(value, left = null, right = null) {
-//     this.value = value;
-//     this.left = left;
-//     this.right = right;
-//   }
-// }
-
-// var sortedArrayToBST = function (nums) {
-//   const build = (start, end) => {
-//     if (start === end) return null;
-//     const mid = Math.floor((start + end) / 2);
-//     const node = new TreeNode(nums[mid]);
-//     node.left = build(start, mid);
-//     node.right = build(mid + 1, end);
-//     return node;
-//   };
-//   return build(0, nums.length);
-// };
-// console.log(sortedArrayToBST([-10, -3, 0, 5, 9]));
-
 class TreeNode {
   constructor(value, left = null, right = null) {
     this.value = value;
@@ -29,7 +8,9 @@ class TreeNode {
 
 function sortedArrayToBST(nums) {
   function buildTree(start, end) {
-    if (start === end) return null;
+    if (start === end) {
+      return null;
+    }
 
     const mid = Math.floor((start + end) / 2);
     let root = new TreeNode(nums[mid]);
@@ -42,4 +23,32 @@ function sortedArrayToBST(nums) {
   return buildTree(0, nums.length);
 }
 
-console.log(sortedArrayToBST([-10, -3, 0, 5, 9]));
+// const tree = sortedArrayToBST([-10, -3, 0, 5, 9]);
+const tree = sortedArrayToBST([1, 3]);
+
+const convertedToArray = treeToArray(tree);
+
+function treeToArray(root) {
+  let result = [];
+  let queue = [root];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+
+    if (node) {
+      result.push(node.value);
+      queue.push(node.left);
+      queue.push(node.right);
+    } else {
+      result.push(null);
+    }
+  }
+
+  while (result[result.length - 1] === null) {
+    result.pop();
+  }
+
+  return result;
+}
+
+console.log("res", convertedToArray);
