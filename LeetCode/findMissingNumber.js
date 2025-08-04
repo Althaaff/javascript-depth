4f560b03fb8b30275ff19cc39efd0b0d73904375
Althaaff <muhammadalthaf228@gmail.com>
2025-08-04 11:33:33 +0530

    Add optimized solution for find missing number leetcode problem

--- LeetCode/findMissingNumber.js
+++ LeetCode/findMissingNumber.js
@@ -1,3 +1,4 @@
+// brute force solution
 function findMissingNumber(nums) {
   if (nums.length === 0) return [];
 
@@ -10,3 +11,15 @@ function findMissingNumber(nums) {
 
 console.log(findMissingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
 console.log(findMissingNumber([0, 1]));
+
+// optimized solution for the above problem :
+function findMissingNum(nums) {
+  const n = nums.length;
+
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = nums.reduce((acc, num) => acc + num, 0);
+
+  return expectedSum - actualSum;
+}
+
+console.log(findMissingNumber([3, 0, 1, 4, 2, 6]));
