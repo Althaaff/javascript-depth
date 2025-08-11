bf5e476a5defb362a3ff50b5a0905c497f0added
Althaaff <muhammadalthaf228@gmail.com>
2025-08-11 20:12:11 +0530

    Add solution for sum of distinct sub array with length k

---
+++ LeetCode/sumOfSubArraySizeK.js
@@ -0,0 +1,36 @@
+// Maximum Sum of Distinct Subarrays With Length K : (sliding window)
+
+function distinctSubArr(arr, k) {
+  if (arr.length < k) return 0;
+
+  let maxSum = 0;
+  let currSum = 0;
+  let set = new Set();
+
+  let left = 0;
+  for (let right = 0; right < arr.length; right++) {
+    while (set.has(arr[right])) {
+      set.delete(arr[left]);
+      currSum -= arr[left];
+      left++;
+    }
+
+    set.add(arr[right]);
+    currSum += arr[right];
+
+    if (right - left + 1 > k) {
+      set.delete(arr[left]);
+      currSum -= arr[left];
+      left++;
+    }
+
+    if (right - left + 1 === k) {
+      maxSum = Math.max(maxSum, currSum);
+    }
+  }
+
+  return maxSum;
+}
+
+console.log(distinctSubArr([3, 5, 3, 4], 2));
+console.log(distinctSubArr([4, 4, 4], 3));
