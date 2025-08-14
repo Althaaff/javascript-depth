db1e861477436f529970acedfb1cf3eee47e3c56
Althaaff <muhammadalthaf228@gmail.com>
2025-08-14 20:26:29 +0530

    Add solution for keyboard rowleetcode problem

---
+++ LeetCode/findWords.js
@@ -0,0 +1,21 @@
+// find words leetcode problem (keyboard row) :
+
+function findWords(words) {
+  let res = [];
+  const rows = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"];
+
+  for (let word of words) {
+    let upperWord = word.toUpperCase();
+
+    for (let row of rows) {
+      if ([...upperWord].every((char) => row.includes(char))) {
+        res.push(word);
+        break;
+      }
+    }
+  }
+
+  return res;
+}
+
+console.log(findWords(["Hello", "Alaska", "Dad", "Peace"]));
