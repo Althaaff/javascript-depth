f17ee5cf76bdd9087fd009ca4660e8623c908488
Althaaff <muhammadalthaf228@gmail.com>
2025-08-15 12:17:13 +0530

    Add solution for teemo attacking leetcode problem

---
+++ LeetCode/teemoAttacking.js
@@ -0,0 +1,11 @@
+function teemoAttacking(timeSeries, duration) {
+  let total = 0;
+
+  for (let i = 0; i < timeSeries.length - 1; i++) {
+    total += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+  }
+
+  return total + duration;
+}
+
+console.log(teemoAttacking([1, 2], 2));
