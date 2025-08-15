9d5476b64af125cb6d51de14a70c8b5b8f0ebcc8
Althaaff <muhammadalthaf228@gmail.com>
2025-08-15 18:09:21 +0530

    Add approach 2 for teemo attacking

--- LeetCode/teemoAttacking.js
+++ LeetCode/teemoAttacking.js
@@ -9,3 +9,26 @@ function teemoAttacking(timeSeries, duration) {
 }
 
 console.log(teemoAttacking([1, 2], 2));
+
+// approach 2:
+
+function teemoAttacking2(timeSeries, duration) {
+  const n = timeSeries.length;
+
+  let totalDuration = 0;
+
+  if (n === 0) return n.length;
+
+  for (let i = 0; i < timeSeries.length - 1; i++) {
+    let timeDefference = timeSeries[i + 1] - timeSeries[i];
+
+    totalDuration += Math.min(duration, timeDefference);
+  }
+
+  totalDuration += duration;
+
+  return totalDuration;
+}
+
+console.log(teemoAttacking2([1, 2, 3], 2)); // poinsons seconds --> 1 -- 2 -- 3 -- 4 // overlapped both 2 and 3
+console.log(teemoAttacking2([1, 2, 6], 2));
