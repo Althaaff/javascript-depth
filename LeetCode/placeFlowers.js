4e47a3183b6406ce78c8b3e2c2f2d17a5481e972
Althaaff <muhammadalthaf228@gmail.com>
2025-08-27 23:56:25 +0530

    Add approach 2 for place flowers leetcode problem

--- LeetCode/placeFlowers.js
+++ LeetCode/placeFlowers.js
@@ -34,3 +34,27 @@ console.log(canPlaceFlowers(flowerbed1, n1));
 const flowerbed2 = [1, 0, 0, 0, 1];
 const n2 = 2;
 console.log(canPlaceFlowers(flowerbed2, n2));
+
+// Approach 2 ( using while loop ) :
+function canPlaceFlowers(flowerbed, n) {
+  let count = 0;
+  let i = 0;
+  while (i < flowerbed.length) {
+    if (
+      flowerbed[i] === 0 &&
+      (i === 0 || flowerbed[i - 1] === 0) &&
+      (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+    ) {
+      flowerbed[i] = 1;
+      count++;
+      i += 2;
+    } else {
+      i++;
+    }
+  }
+
+  // console.log("count", count);
+  return count >= n;
+}
+
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2));
