15e586f12e88defae6083c06fd7a280375565dfd
Althaaff <muhammadalthaf228@gmail.com>
2025-08-27 23:47:36 +0530

    Add solution for can place flowers leetcode problem using recusrion

---
+++ LeetCode/placeFlowers.js
@@ -0,0 +1,36 @@
+// using recusrion :
+function canPlaceFlowers(flowerbed, n) {
+  function canPlant(index, n) {
+    if (n === 0) {
+      return true;
+    }
+    if (index >= flowerbed.length) {
+      return false;
+    }
+    if (flowerbed[index] === 0) {
+      const leftEmpty = index === 0 || flowerbed[index - 1] === 0;
+      const rightEmpty =
+        index === flowerbed.length - 1 || flowerbed[index + 1] === 0;
+
+      if (leftEmpty && rightEmpty) {
+        flowerbed[index] = 1;
+        if (canPlant(index + 1, n - 1)) {
+          return true;
+        }
+        flowerbed[index] = 0;
+      }
+    }
+
+    return canPlant(index + 1, n);
+  }
+
+  return canPlant(0, n);
+}
+
+const flowerbed1 = [1, 0, 0, 0, 1];
+const n1 = 1;
+console.log(canPlaceFlowers(flowerbed1, n1));
+
+const flowerbed2 = [1, 0, 0, 0, 1];
+const n2 = 2;
+console.log(canPlaceFlowers(flowerbed2, n2));
