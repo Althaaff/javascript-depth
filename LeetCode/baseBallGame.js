cd73c42735bd86e129d627f6203960b8d619a708
Althaaff <muhammadalthaf228@gmail.com>
2025-09-17 19:15:54 +0530

    Add solution for base ball game leetcode problem

---
+++ LeetCode/baseBallGame.js
@@ -0,0 +1,36 @@
+function baseBallGame(operations) {
+  let totalSum = 0;
+  let records = [];
+
+  operations.forEach((item) => {
+    let convertedToNum = Number(item);
+
+    if (!isNaN(convertedToNum)) {
+      records.push(convertedToNum);
+    }
+
+    // logics should goes here :
+    if (item === "C") {
+      records.pop();
+    } else if (item === "D") {
+      records[records.length] = records[records.length - 1] * 2;
+    } else if (item === "+") {
+      records[records.length] =
+        records[records.length - 1] + records[records.length - 2];
+    }
+  });
+
+  if (records.length === 0) {
+    records.push(0);
+  }
+
+  for (let i = 0; i < records.length; i++) {
+    totalSum += records[i];
+  }
+
+  return totalSum;
+}
+
+console.log(baseBallGame(["5", "2", "C", "D", "+"]));
+console.log(baseBallGame(["5", "-2", "4", "C", "D", "9", "+", "+"]));
+console.log(baseBallGame(["1", "C"]));
