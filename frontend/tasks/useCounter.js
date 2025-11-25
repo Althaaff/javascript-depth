8f7d71feb64bdcab918a3922536454f4fe8f36f6
Althaaff <muhammadalthaf228@gmail.com>
2025-11-25 12:01:59 +0530

    Implement a useCounter hook that manages a counter state

---
+++ frontend/tasks/useCounter.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function useCounter(initialValue = 0) {
+  const [count, setCount] = useState(initialValue);
+
+  const increment = () => {
+    setCount((count) => count + 1);
+  };
+
+  const decrement = () => {
+    setCount((count) => count - 1);
+  };
+
+  const reset = () => {
+    setCount(initialValue);
+  };
+
+  return { count, increment, decrement, reset, setCount };
+}
+
+const { count, increment, decrement, reset } = useCounter();
+
+// log :
+console.log(count);
