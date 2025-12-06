7a80e01cc694ea3244cf73934cb8bf4e43c43b7b
Althaaff <muhammadalthaf228@gmail.com>
2025-12-06 20:47:38 +0530

    Implement a function that performs a bubble sort using recursion

--- frontend/tasks/bubbleSort.js
+++ frontend/tasks/bubbleSort.js
@@ -6,9 +6,7 @@ function bubbleSort(nums) {
 
     for (let j = 0; j <= n - i - 2; j++) {
       if (nums[j] > nums[j + 1]) {
-        console.log("compare", nums[j], nums[j + 1]);
         [nums[j + 1], nums[j]] = [nums[j], nums[j + 1]];
-        console.log("nums", nums);
         swapped = true;
       }
     }
@@ -22,3 +20,25 @@ function bubbleSort(nums) {
 }
 
 console.log(bubbleSort([9, 3, 6, 2, 1, 11]));
+
+// using recursion :
+function bubbleSortRecursion(nums) {
+  let shouldSort = false;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    let curr = nums[i];
+
+    if (curr > nums[i + 1]) {
+      curr = nums[i + 1];
+      nums[i + 1] = curr;
+      shouldSort = true;
+    }
+  }
+
+  if (shouldSort) {
+    bubbleSort(nums);
+  }
+  return nums;
+}
+
+console.log(bubbleSortRecursion([9, 3, 6, 2, 1, 11]));
