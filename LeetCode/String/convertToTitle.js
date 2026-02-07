3b0ed38ae5ccc7277c8b4ea4f4318851120ce9f1
Althaaff <muhammadalthaf228@gmail.com>
2026-02-07 12:09:18 +0530

    Add solution for convert to title leetcode problem

--- LeetCode/String/convertToTitle.js
+++ LeetCode/String/convertToTitle.js
@@ -4,12 +4,15 @@ function convertToTitle(columnNumber) {
   while (columnNumber > 0) {
     columnNumber--; // decrement by 1 to make it 0-indexed
 
+    // get remainder (0-25)
     let remainder = columnNumber % 26;
 
+    // convert to character (0 = 'A', 25 = 'Z')
     let char = String.fromCharCode(65 + remainder);
 
     res = char + res;
 
+    // move to the next digit:
     columnNumber = Math.floor(columnNumber / 26);
   }
 
